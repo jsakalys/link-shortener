18d7fc1a9a407f2f263af2c7422e4fda4bb881bc
JSAKALYS <TASHIKANAKOTO@GMAIL.COM>
2015-10-23 08:51:17 -0700

    Parts 1 and 2 complete at this time

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ app.use(ejsLayouts);
 var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 
+var Hashids = require("hashids"),
+    hashids = new Hashids("gyro");
+ 
+
 app.set('view engine', 'ejs');
 
 app.get('/', function(req, res){
@@ -15,22 +19,38 @@ app.get('/', function(req, res){
 	res.render('index');
 });
 
+app.get('/links', function(req, res){
+	res.render('links/list');
+});
+
 app.post('/links/', function(req, res) {
 //accepts data from the form
 	var data = req.body.q;
 //stores the url in the database
-	db.link.create({ url: data, hash: '' }).then(function(link) {
-
-//redirects the user to the show page
-	res.redirect('/links/' + link.id);
-
-});
+	db.link.create({ url: data }).then(function(link) {
+		var hashID = hashids.encode(link.id);
+		link.hash = hashID;
+		link.save().then(function() {
+		//redirects the user to the show page
+		res.redirect('/links/' + link.id);
+		});
+	});
 });
 
 app.get('/links/:id', function(req, res) {
 	var linkID = parseInt(req.params.id);
-	db.link.findById(linkID).then(function(foundLink) {
-	res.render('links/show', {item: foundLink});
+	db.link.findById(linkID).then(function(link) {
+	res.render('links/show', {item: link});
+	});
+});
+
+app.get('/:hash', function(req, res){
+	var hash = req.params.hash;
+	db.link.find({where: {hash: hash}}).then(function(link) {
+		link.count++;
+		link.save().then(function(){
+		res.redirect('http://' + link.url);		
+		})
 	});
 });
 
---
+++ migrations/20151022194830-unnamed-migration.js
@@ -0,0 +1,11 @@
+'use strict';
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+      return queryInterface.addColumn('links', 'count', Sequelize.INTEGER);
+  },
+
+  down: function (queryInterface, Sequelize) {
+      return queryInterface.removeColumn('links', 'count')
+  }
+};
